e630b32e523a941121b2e0e6cf02c5a6e0aab79e
ttsgosadow <ttsgosadow@gmail.com>
2016-12-17 15:33:22 +0100

    Update gameGen.js

--- bootstrap/gameGen.js
+++ bootstrap/gameGen.js
@@ -107,7 +107,7 @@ function togglediv(id) {
                 var generatedGame = '<div class="alert alert-success" role="alert"><strong>Succes!</strong> The fickle winds of fate yielded these flips for deployment, strategy and schemes.</div>' + 
                     '<div class="panel panel-primary"><div class="panel-heading"><h3 class="panel-title">'+
                     '<strong>Game: ' + cardImage(deploymentCard) + ' ' + cardImage(strategyCard) + ' ' + cardImage(schemeCard1) + ' ' + cardImage(schemeCard2) + '</strong>' +
-                    ' [<a href="' + gameLink + '">perma-link</a>]' + 
+                    ' <mark>[<a href="' + gameLink + '">perma-link</a>]</mark>' + 
                     '</h3></div><div class="panel-body">' +
                     '<span class="text-right"><a href="#" onClick="toggleAll(true);return false;">Show</a>/<a href="#" onClick="toggleAll(false);return false;">hide</a> all</span>' +
                     '<p><strong>Deployment:</strong> ' + deployment[0] + ' (' + cardImage(deploymentCard) + ')<input type="checkbox" class="toggle" id="deployment-text-cb" '+checkedState+' onclick="togglediv(\'deployment-text\')"><div class="deployment-text" id="deployment-text">' + deployment[1] + '</div></p>' +
