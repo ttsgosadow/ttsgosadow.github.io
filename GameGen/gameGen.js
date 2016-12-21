501b7c8e4844ad174308f6fa052fb306835fd1f4
ttsgosadow <ttsgosadow@gmail.com>
2016-12-21 14:26:27 +0100

    Update gameGen.js

--- GameGen/gameGen.js
+++ GameGen/gameGen.js
@@ -122,7 +122,7 @@ function togglediv(id) {
                     'to share the game setup.</p>' +
                     '<!-- Modal --><div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"><div class="modal-dialog" role="document"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>' +
                     '<h4 class="modal-title" id="myModalLabel">Copy Perma-link</h4></div>' +
-                    '<div class="modal-body"><span id="visible-input">https://ttsgosadow.github.com/GameGen/'+gameLink+'</span></div>
+                    '<div class="modal-body"><span id="visible-input">https://ttsgosadow.github.com/GameGen/' + gameLink + '</span></div>' +
                     '<div class="modal-footer"><button type="button" class="btn btn-primary btn-clipboard" onClick="new Clipboard(\'.btn-clipboard\');" data-clipboard-target="#visible-input">Copy to clipboard</button><button type="button" class="btn btn-default" data-dismiss="modal">Close</button></div></div></div></div>' +
                     '<div class="panel panel-default">' +
                     '<div class="panel-heading"><h3 class="panel-title">'+
