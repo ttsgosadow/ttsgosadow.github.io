93bcf4e33616c1f54688eaa2252c8031b83a9b26
ttsgosadow <ttsgosadow@gmail.com>
2016-12-21 12:35:20 +0100

    Update gameGen.js

--- GameGen/gameGen.js
+++ GameGen/gameGen.js
@@ -1,3 +1,7 @@
+$(function () {
+  $('[data-toggle="popover"]').popover()
+})
+
 function togglediv(id) {
                 var div = document.getElementById(id);
                 if (document.getElementById(id + '-cb').checked)
