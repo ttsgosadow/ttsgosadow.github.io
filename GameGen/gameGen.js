51ec61a38580ec5d28a6f950fc08a655d5e66309
ttsgosadow <ttsgosadow@gmail.com>
2017-02-24 22:02:13 +0100

    Update gameGen.js

--- GameGen/gameGen.js
+++ GameGen/gameGen.js
@@ -92,7 +92,7 @@ function shuffle(array)
   // While there remain elements to shuffle...
   while (0 !== currentIndex) {
       // Pick a remaining element...
-      randomIndex = Math.floor(Math.random() * currentIndex);
+      randomIndex = Math.floor(Math.random() * array.length);
       currentIndex -= 1;
       // And swap it with the current element.
       temporaryValue = array[currentIndex];
