e2a9c0b056c199ac527e2857b2b1a447bbda3c83
ttsgosadow <ttsgosadow@gmail.com>
2017-05-25 07:43:06 +0200

    Update PfW.js

--- PfWLeague/PfW.js
+++ PfWLeague/PfW.js
@@ -2,14 +2,14 @@ function getPlayers()
 {
    var players = [
    [ "Arne - Ten Thunders", ['c',1] , [2,1] , [13,1] ],
-   [ "Gjalt - Neverborn", ['m',5] , ['m',1], ['r',2], ['t',1], [1,1], [11,3] ],
+   [ "Gjalt - Neverborn", ['m',5] , ['m',1], ['r',2], ['t',1], [1,3], [11,3] ],
    [ "Jeroen - Neverborn", ['c',1] , [7,1] , ['rj',3] ],
    [ "Jim - Guild", ['r',1] , ['r',1] , ['m',1] ],
    [ "Kasper - Ten Thunders", ['c', 1] , [13,3], [11,1] ],
    [ "Patrick - Ten Thunders", ['c',3] , [7,1] , [9,1] ],
    [ "Robin - Arcanists" , [1,3] , [2,1], ['placeholder',1] ],
    [ "Sander - Outcasts", ['c',1] , ['r',1] , ['t',1] ],
-   [ "Tim - Resurrectionists", ['c',1] , ['t',1] , [13,1] ]
+   [ "Tim - Resurrectionists", ['c',3] , ['c',1] , [13,1] ]
   ]
    
   return players
