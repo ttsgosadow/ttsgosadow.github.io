9e1038378fa1c378528c2386d16fae2e3d983610
ttsgosadow <ttsgosadow@gmail.com>
2017-08-01 14:39:58 +0200

    Update PfW.js

--- PfWLeague/PfW.js
+++ PfWLeague/PfW.js
@@ -17,7 +17,7 @@ function getPlayers()
 
 function getRanking()
 {
-   var lastUpdated = "9 July, 2017"
+   var lastUpdated = "1 August, 2017"
  var players = getPlayers()
  var playerRankings = new Array()
  for( i = 0; i < players.length; i++ )
