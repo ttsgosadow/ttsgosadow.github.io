5106e1022e24176c9c33a63c6c1ace30526563de
ttsgosadow <ttsgosadow@gmail.com>
2018-07-23 10:17:05 +0200

    Update specGame.js

--- GameGen/specGame.js
+++ GameGen/specGame.js
@@ -1,1 +1,54 @@
+function specGame( gameType )
+{
+  var specifyGame = 'Incompatible Game type - please try again.'
+  if( gameType == 'GG18' )
+  {
+    var specifyGame = '<p>You have specified a GG18 game.</p>'
+    
+    specifyGame += ddlbDeployment( gameType )
+    specifyGame += "<br />"
+    specifyGame += ddlbstrategy( gameType )
+    specifyGame += "<br />"
+    specifyGame += ddlbSchemes( gameType )
+    specifyGame += "<br />"
+    specifyGame =+ buttonGO( 4 )
+  }
+  
+  document.getElementById( targetField ).innerHTML = specifyGame
+}
 
+function ddlbDeployment( gameType )
+{
+  var depl ='<select id="specDeployment" name="specDeployment">'
+  var deck = newDeck()
+  var charCards = getCharCards()
+  var n = 0
+  while( n < deck.length )
+  {
+    var depl += '<option value="'+charCards[deck[n]]+'">deck[n]</option>'
+    n++
+  }
+  
+  var depl += '</select>'
+  return depl
+}
+
+function ddlbStrategy( gameType )
+{
+  
+}
+
+function ddlbSchemes( gameType )
+{
+  
+}
+
+function buttonGO( numSchemes)
+{
+  
+}
+
+function startSpecGame()
+{
+  
+}
